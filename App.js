656f3fa62209448521e46f763985d953403d80e7
MartinCypher <malevouy@gmail.com>
2024-01-23 15:05:02 -0300

    Subiendo mi Preentrega

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import HolaCoder from "./src/Components/HolaCoder"
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <Text>¡Hola Tutores!</Text>
+    <HolaCoder/>
+    <StatusBar style="auto" />
     </View>
   );
 }
