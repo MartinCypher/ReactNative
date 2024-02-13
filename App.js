15415199d78a1522d7a8f9b64f6c421fb1b8ec24
MartinCypher <malevouy@gmail.com>
2024-02-13 17:08:38 -0300

    3era Preentrega

--- App.js
+++ App.js
@@ -1,12 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import TodoList from './src/Components/Lista';
+import { useState } from "react";
+import { View, StyleSheet } from "react-native";
+import { useFonts } from "expo-font";
+import Constants from "expo-constants";
+import Home from "./src/screens/Home";
+import ItemListCategories from "./src/screens/ItemListCategories";
+import { fonts } from "./src/global/fonts";
+import { StatusBar } from "expo-status-bar";
 
 export default function App() {
+  const [fontsLoaded] = useFonts(fonts);
+
+  const [categorySelected, setCategorySelected] = useState("");
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
-    <View style={{flex:1}}>
-      <TodoList/>
+    <View style={styles.container}>
+      <StatusBar style="auto" />
+      {categorySelected ? (
+        <ItemListCategories category={categorySelected} />
+      ) : (
+        <Home setCategorySelected={setCategorySelected} />
+      )}
     </View>
   );
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#ededed",
+    alignItems: "center",
+    paddingTop: Constants.statusBarHeight,
+  },
+});
